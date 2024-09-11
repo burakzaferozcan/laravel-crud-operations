035710ee713d4937c30ebf9d6d91aa5e5d28798c
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-11 16:47:36 +0300

    create view and route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends("layout")
+
+@section("content")
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">ÜRÜN ADI</th>
+            <th scope="col">AÇIKLAMA</th>
+            <th scope="col">FİYAT</th>
+            <th scope="col">AYRINTI GÖSTER</th>
+            <th scope="col">DÜZENLE</th>
+            <th scope="col">SİL</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        </tbody>
+    </table>
+@endsection
+
